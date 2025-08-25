dd4101a35cd02724ed22627efe4433b5fb5d0645
Gire <airat@gus>
2025-08-25 15:38:16 +0300

    fixed compose and entrypoint

--- mycloud/backend/entrypoint.sh
+++ mycloud/backend/entrypoint.sh
@@ -26,7 +26,7 @@ python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 
 if [ "$DJANGO_SUPERUSER_USERNAME" ] && [ "$DJANGO_SUPERUSER_PASSWORD" ]; then
-  python manage.py createsuperuser --noinput --username $DJANGO_SUPERUSER_USERNAME --email ${DJANGO_SUPERUSER_EMAIL:-admin@example.com} || true
+  python manage.py createsuperuser --noinput --username "$DJANGO_SUPERUSER_USERNAME" --email "${DJANGO_SUPERUSER_EMAIL:-admin@example.com}" || true
 fi
 
 python manage.py runserver 0.0.0.0:8000
